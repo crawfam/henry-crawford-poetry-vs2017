074ab588bc7b8753c3059540eaadc8ac176fcf9e
Henry Crawford <crawfam@gmail.com>
2020-10-08 11:53:28 -0400

    Added JeffCrawford page

---
+++ HenryCrawfordPoetry/Controllers/JeffCrawfordController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HenryCrawfordPoetry.Controllers
+{
+    public class JeffCrawfordController : Controller
+    {
+     
+        // GET: /Jeff Crawford/
+        public ActionResult Index()
+        {
+            return View("Index", "_LayoutProfJeff");
+        }
+
+    }
+}
